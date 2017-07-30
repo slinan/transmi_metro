eb46decc6bfa8958bb69c4d4beb4c03f8bb88c50
slinan <s.linan10@uniandes.edu.co>
2017-07-30 11:11:35 -0500

    Documentación

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,16 +16,19 @@ import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 
-
+/**
+  * Main application
+  * @param ws Injected web socket
+  */
 class Application @Inject()(ws: WSClient) extends Controller {
 
-  // http endpoint to check that the server is running
+  // HTTP endpoint to check that the server is running
   def index = Action {
-    Ok("I'm alive!\n")
+    Ok("OK\n")
   }
 
-  // sends the time every second, ignores any input
-  def wsPingPong = WebSocket.using[String] {
+  // Sends the content of the main queue
+  def socket = WebSocket.using[String] {
     request =>
       Logger.info(s"Client Connected")
       StreamManager.queue.clear()
@@ -35,50 +38,57 @@ class Application @Inject()(ws: WSClient) extends Controller {
 
       }, 20))
 
-      val inIteratee: Iteratee[String, Unit] = Iteratee.foreach[String](receivedString => {
-        Main.killAll()
-        StreamManager.queue.clear()
-      })
-
       (Iteratee.ignore[String], outEnumerator)
-      (inIteratee, outEnumerator)
-
   }
 }
 
-object RoutesBusEnum extends Enumeration{
-  type RoutesBusEnum = Value
-  val GREEN_EAST, GREEN_WEST, RED_NORTH, RED_SOUTH, LONG_EAST, LONG_NORTH = Value
-}
-
+/**
+  * Manages the stream of events that the socket will read
+  */
 object StreamManager {
   var queue = new ConcurrentLinkedQueue[String]
   queue.add("Starting")
   def poll(): String = synchronized{queue.poll()}
-  def add(str:String) = synchronized{
-    queue.add(str)
+  def add(str:String) = synchronized{queue.add(str)
   }
 }
 
-class Bus (nIdBus: Int,delay: Long, startingDelay: Long,nStations:List[Int],nSize:Int, startAgainDelay: Long) extends Runnable{
-  def size:Int = nSize
-  def idBus:Int = nIdBus
-  def stations:List[Int]=nStations
-  def numberStations: Int = stations.length
-  var ocupation: Int = 0
+/**
+  * Represents a bus inside the system. Extends Runnable
+  * @param pIdTrain ID of the train
+  * @param pDelay Delay of the train between stations
+  * @param pStartingDelay Delay to start the train
+  * @param pStations Array of the stations that the train will go over
+  * @param pSize Maximum size of the train
+  * @param pStartAgainDelay Delay between the train stops its route and starts again
+  */
+class Bus (pIdTrain: Int, pDelay: Long, pStartingDelay: Long, pStations:List[Int], pSize:Int, pStartAgainDelay: Long) extends Runnable{
+  def size:Int = pSize
+  def idTrain:Int = pIdTrain
+  def stations:List[Int]=pStations
+  var occupation: Int = 0 //Ocupation of the train
 
+  //Linked hash map of the queues. There is a queue for each train station
   var queues: mutable.LinkedHashMap[Int,Int] = new mutable.LinkedHashMap()
   for (i <- 0 to 14) {
     queues.put(i+1,0)
   }
 
+  /**
+    * Defines the direction of the train.
+    * @return 1 if goes from East to North or East to West, -1 if goes from North to East or West to East
+    */
   def direction():Int = {
-    if (nStations(1) > nStations(2)) {
-      -1
-    }
+    if (pStations(1) > pStations(2)) {-1}
     1
   }
 
+  /**
+    * Defines if a station is after another, given the direction of the train
+    * @param station First station to test
+    * @param stationNext Second station to test
+    * @return true if stationNext if after station, false otherwise
+    */
   def isStationNext(station: Int, stationNext: Int): Boolean =
   {
     if((direction== 1 && station < stationNext) || (direction== -1 && station > stationNext))
@@ -89,59 +99,67 @@ class Bus (nIdBus: Int,delay: Long, startingDelay: Long,nStations:List[Int],nSiz
       {
         false
       }
-
     true
-
   }
 
+  /**
+    * Runnable method of the bus
+    */
   def run: Unit = {
-    Thread.sleep(startingDelay)
+    Thread.sleep(pStartingDelay) //Waits the starting delay
 
     while(Main.runTrains) {
-      StreamManager.add("TRAIN_STATUS:" + idBus + ":On Route")
-      for (currentStation <- nStations if Main.runTrains) {
+      StreamManager.add("TRAIN_STATUS:" + idTrain + ":On Route") //Prints in the queue On Route state
+      //Walks for every station inside the list
+      for (currentStation <- pStations if Main.runTrains) {
         var station: Station = Main.stationsMap(currentStation)
-        StreamManager.add("ARRIVED:" + idBus + ":" + currentStation)
-
+        StreamManager.add("ARRIVED:" + idTrain + ":" + currentStation) //Alert that the train has arrived
 
         //Pick up passengers
-
-        for (nextStation <- nStations if size >= ocupation && isStationNext(currentStation, nextStation) if Main.runTrains) {
-          var newUsers: Int = station.pickUp(nextStation, size - ocupation)
-
-          if (newUsers > 0) {
-            addUsers(newUsers, nextStation)
-          }
+        for (nextStation <- pStations if size >= occupation && isStationNext(currentStation, nextStation) if Main.runTrains) {
+          var newUsers: Int = station.pickUp(nextStation, size - occupation)
+          if (newUsers > 0) {addUsers(newUsers, nextStation)}
         }
 
-        //Leave passengers
+        //Leave passengers in the station
         var leavingUsers: Int = queues(currentStation)
         queues.put(currentStation, 0)
-        ocupation -= leavingUsers
-        StreamManager.add("TRAIN:" + idBus + ":" + ocupation)
+        occupation -= leavingUsers
+        StreamManager.add("TRAIN:" + idTrain + ":" + occupation) //Alert the train's occupation
         station.addLeavingUsers(leavingUsers)
 
         //Time between stations
-        Thread.sleep(delay)
+        Thread.sleep(pDelay)
       }
 
-      StreamManager.add("TRAIN_STATUS:" + idBus + ":Returning to initial station")
-      Thread.sleep(startAgainDelay)
+      // If the train ends its route, the train returns to its home station
+      StreamManager.add("TRAIN_STATUS:" + idTrain + ":Returning to initial station")
+      Thread.sleep(pStartAgainDelay)
     }
   }
 
-  def addUsers(number:Int, destination: Int): Boolean =
+  /**
+    * Add a number of users to its waiting queue
+    * @param number Number of users that will be added
+    * @param destination Destination of the users group
+    */
+  def addUsers(number:Int, destination: Int): Unit =
   {
-      ocupation += number
+      occupation += number
       queues(destination) += number
-      StreamManager.add("TRAIN:" + idBus + ":" + ocupation)
+      StreamManager.add("TRAIN:" + idTrain + ":" + occupation)
   }
 }
 
-class Station (nIdStation: Int) extends Runnable
+/**
+  * Represents a station inside the system
+  * @param pIdStation Id of the station
+  */
+class Station (pIdStation: Int) extends Runnable
 {
-  def idStation:Int = nIdStation
+  def idStation:Int = pIdStation
 
+  //Linked hash map of waiting queues inside the station
   var queues: mutable.LinkedHashMap[Int, Int] = new mutable.LinkedHashMap()
   var users = 0
   var usersLeaving = 0
@@ -150,64 +168,80 @@ class Station (nIdStation: Int) extends Runnable
     queues.put(i+1,0)
   }
 
-  var queueLeaving: ConcurrentLinkedQueue[People] = new ConcurrentLinkedQueue[People]()
+  //Concurrent queue of people entering the station
+  var queueEntering: ConcurrentLinkedQueue[People] = new ConcurrentLinkedQueue[People]()
 
-  def addUsers(number:Int, destination: Int): Unit = synchronized
+  /**
+    * Adds a number of user to a specific destination
+    * @param pNumber Number of users to be added
+    * @param pDestination Destination of the users
+    */
+  def addUsers(pNumber:Int, pDestination: Int): Unit = synchronized
   {
-    usersComing +=number
-    if(destination == idStation)
-    {
-      deleteUsers(number)
-    }
+    usersComing +=pNumber
+    if(pDestination == idStation) {deleteUsers(pNumber)} //If home station is the same destination, the user has arrived.
     else {
-      queues(destination) += number
-      users += number
+      queues(pDestination) += pNumber
+      users += pNumber
       StreamManager.add("STATION:" + idStation + ":" + users)
     }
   }
 
-  def deleteUsers(number: Int): Unit = synchronized
+  /**
+    * Delete the people from the system
+    * @param pNumber Number of people to be deleted
+    */
+  def deleteUsers(pNumber: Int): Unit = synchronized
   {
-    users -= number
-    usersLeaving += number
+    users -= pNumber
+    usersLeaving += pNumber
     StreamManager.add("STATION:"+idStation+":"+users)
   }
 
-  def addLeavingUsers(number: Int): Unit = synchronized
+  /**
+    * Sums the leaving users counter
+    * @param pNumber Number of users that will be leaving
+    */
+  def addLeavingUsers(pNumber: Int): Unit = synchronized
   {
-    usersLeaving += number
+    usersLeaving += pNumber
     StreamManager.add("STATION:"+idStation+":"+users)
   }
 
-  def pickUp(destination:Int, availableSeats:Int): Int = synchronized
+  /**
+    * PickUp a number of passengers given a number of available seats
+    * @param pDestination Destination of the users
+    * @param pAvailableSeats Available places in the bus
+    * @return Number of passengers that will leave the station
+    */
+  def pickUp(pDestination:Int, pAvailableSeats:Int): Int = synchronized
   {
     var passengers = 0
-    if(availableSeats >= queues(destination)) {
-      passengers = queues(destination)
+    if(pAvailableSeats >= queues(pDestination)) {
+      passengers = queues(pDestination)
     }
     else {
-      passengers = availableSeats
+      passengers = pAvailableSeats
     }
 
-    queues(destination) -=  passengers
+    queues(pDestination) -=  passengers
     users -= passengers
     usersLeaving += passengers
     passengers
   }
 
-
+  // Run method of the thread
   def run: Unit = {
 
     var count: Int = 0
     while(Main.runTrains) {
 
-
-      var people: People = queueLeaving.peek()
+      var people: People = queueEntering.peek()
       while (people != null && count == people.minutoLlegada && Main.runTrains)
         {
-          queueLeaving.poll()
+          queueEntering.poll()
           addUsers(people.numPersonas, people.destino)
-          people = queueLeaving.peek()
+          people = queueEntering.peek()
         }
       count += 1
 
@@ -224,29 +258,46 @@ class Station (nIdStation: Int) extends Runnable
 
 }
 
-class Reporter(time:Long) extends Runnable {
+/**
+  * Reports the system statics
+  * @param pTime Time between reports
+  */
+class Reporter(pTime:Long) extends Runnable {
 
   def run: Unit = {
 
     while (Main.runTrains) {
-      Thread.sleep(time)
+      Thread.sleep(pTime)
       Main.stationsMap.foreach(_._2.reportStatistics)
     }
 
   }
 }
 
-class People(pNumPersonas: Int, pDestino: Int, pOrigen: Int, pMinutoLlegada: Int )
+/**
+  * Representation of a entry in the CSV file
+  * @param pNumPeople Number of people
+  * @param pDestination Destination station
+  * @param pOrigin Origin station
+  * @param pTime Minute when the users will arrive
+  */
+class People(pNumPeople: Int, pDestination: Int, pOrigin: Int, pTime: Int )
 {
-  def numPersonas:Int = pNumPersonas
-  def destino:Int = pDestino
-  def origen:Int = pOrigen
-  def minutoLlegada = pMinutoLlegada
+  def numPersonas:Int = pNumPeople
+  def destino:Int = pDestination
+  def origen:Int = pOrigin
+  def minutoLlegada = pTime
 
 }
 
-
+/**
+  * Main object that contains the simulation
+  */
 object Main {
+
+  /**
+    * Kill the simulation when the signal is received
+    */
   def killAll(): Unit = {
     runTrains = false
     var i: Int = 0
@@ -256,24 +307,33 @@ object Main {
     }
   }
 
+  /**
+    * Threads array
+    * @return Array of threads
+    */
   def threads: ConcurrentLinkedQueue[Thread] =new ConcurrentLinkedQueue[Thread] ()
 
+  //Possible routes of the trains
   val route1: List[Int] = List[Int](1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   val route2: List[Int] = List[Int](10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
   val route3: List[Int] = List[Int](1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
   val route4: List[Int] = List[Int](15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
   val route5: List[Int] = List[Int](15, 14, 13, 12, 11, 10)
   val route6: List[Int] = List[Int](10, 11, 12, 13, 14, 15)
-
   val baseUnit = 4000
   val delay: Long = 4*baseUnit
   val reportDelay: Long = 10*baseUnit
   val startAgainDelay = 10*baseUnit
 
+  //Hash map of buses
   var busesMap: mutable.LinkedHashMap[Int,Bus] = new mutable.LinkedHashMap()
+  //Hash map of stations
   var stationsMap: mutable.LinkedHashMap[Int, Station] = mutable.LinkedHashMap()
   var runTrains: Boolean = true
 
+  /**
+    * Starts the simulation
+    */
   def start(): Unit = {
     println("START MAIN")
     createStationsMap()
@@ -285,12 +345,18 @@ object Main {
     threads.add(thReporter)
   }
 
+  /**
+    * Create the map of stations
+    */
   def createStationsMap(): Unit = {
     for (i <- 0 to 14) {
       stationsMap.put(i+1,new Station((i + 1)))
     }
   }
 
+  /**
+    * Starts the stations threads
+    */
   def startStations(): Unit = {
     for (i <- 0 to 14) {
       var thStation: Thread = new Thread (stationsMap(i+1))
@@ -299,26 +365,32 @@ object Main {
     }
   }
 
+  /**
+    * Read people from the CSV file
+    */
   def readPeople(): Unit = {
     val bufferedSource = Source.fromFile("filePeople.csv")
     for (line <- bufferedSource.getLines) {
       val cols = line.split(",").map(_.trim)
-      var numPersonas: Int = 0
-      var destino: Int = 0
-      var origen: Int = 0
-      var minutoLlegada: Int = 0
+      var numPeople: Int = 0
+      var destination: Int = 0
+      var origin: Int = 0
+      var trainArrival: Int = 0
       if(!cols(0).contains("num_personas")) {
-        numPersonas = cols(0).toInt
-        destino = cols(1).toInt
-        origen = cols(2).toInt
-        minutoLlegada = cols(3).toInt
-        if(origen != destino) {
-          stationsMap(origen).queueLeaving.add(new People(numPersonas, destino, origen, minutoLlegada))
+        numPeople = cols(0).toInt
+        destination = cols(1).toInt
+        origin = cols(2).toInt
+        trainArrival = cols(3).toInt
+        if(origin != destination) {
+          stationsMap(origin).queueEntering.add(new People(numPeople, destination, origin, trainArrival))
         }
       }
       }
   }
 
+  /**
+    * Read the buses CSV file
+    */
   def readBuses(): Unit = {
     val bufferedSource = Source.fromFile("busSchedules.csv")
     for (line <- bufferedSource.getLines) {
@@ -366,8 +438,5 @@ object Main {
     }
     bufferedSource.close
   }
-
-
-
 }
 
